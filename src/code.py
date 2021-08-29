4e37cbfdd6f9b297d5b938969b4702aa9418c94d
hawat629 <58569872+hawat629@users.noreply.github.com>
2021-08-29 16:38:48 +0200

    start structure the class, check type of input data set

--- src/code.py
+++ src/code.py
@@ -1,6 +1,16 @@
-import pandas as pd
+import src.utils as utils
 
-df = pd.read_csv("logs.csv")  # read data
 
-df = df.set_index("d")  # to re-index with a column 'd'
-df = df.sort_index()  # to sort with respect to the index
+class ImproveDataQuality:
+    """Implementation of various method for improving data quality of a given data"""
+
+
+def __init__(self, data):
+    """
+    Args:
+        data (CSV, JSON, SQL): data set.
+    """
+    if utils.check_extension(data) == "none":
+        raise TypeError("data should be of provided as .csv or .json or .sql file")
+
+    self.data = utils._to_DataFrame(data)

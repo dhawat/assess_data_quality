4217d405c2ed9593e87467277f5a2d4e53f95937
MehdiBits <78798077+MehdiBits@users.noreply.github.com>
2021-08-30 11:39:38 +0200

    Updated for pandas use and xlsx

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,16 @@
 import pandas as pd
+import re
 
 """df = pd.read_csv("logs.csv")  # read data
 
 df = df.set_index("d")  # to re-index with a column 'd'
 df = df.sort_index()  # to sort with respect to the index """
 
-import re
+
 
 
 def check_extension(data):
-    """check if the expension of data is within CSV, JSON or SQL
+    """check if the extension of data is within CSV, JSON or SQL
 
     Args:
         data (): data set
@@ -19,15 +20,17 @@ def check_extension(data):
     """
     if re.search("\.csv$", data, flags=re.IGNORECASE):
         return "csv"
-    if re.search("\.json$", fname, flags=re.IGNORECASE):
+    if re.search("\.json$", data, flags=re.IGNORECASE):
         return "json"
-    if re.search("\.sql$", fname, flags=re.IGNORECASE):
+    if re.search("\.sql$", data, flags=re.IGNORECASE):
         return "sql"
+    if re.search("\.xlsx$", data, flag=re.IGNORECASE):
+        return "xlsx"
     return "none"
 
 
 def _to_DataFrame(data):
-    """read data and transformit to DataFrame
+    """read data and transform it to DataFrame
 
     Args:
         data (CSV, JSON, SQL): data
@@ -35,4 +38,8 @@ def _to_DataFrame(data):
     Returns:
         Dataframe
     """
-    return data
+
+    ext = check_extension(data)
+    f_dict = {'csv': pd.read_csv, 'json': pd.read_json, 'sql': pd.read_sq, 'xlsx': pd.read_excel}
+    df = f_dict[ext]()
+    return df

b553b567acec036f0375db393d7b93bc91c7a8a8
hawat629 <58569872+hawat629@users.noreply.github.com>
2021-08-30 22:55:08 +0200

    modify docs

--- src/improve_data_quality.py
+++ src/improve_data_quality.py
@@ -4,13 +4,12 @@ import utils as utils
 class ImproveDataQuality:
     """Implementation of various method for improving data quality of a given data"""
 
+    def __init__(self, data):
+        """
+        Args:
+            data (CSV, JSON, SQL, xlsx): data set.
+        """
+        if utils.check_extension(data) == "none":
+            raise TypeError("data should be of provided as .csv or .json or .sql file")
 
-def __init__(self, data):
-    """
-    Args:
-        data (CSV, JSON, SQL): data set.
-    """
-    if utils.check_extension(data) == "none":
-        raise TypeError("data should be of provided as .csv or .json or .sql file")
-
-    self.data = utils._to_DataFrame(data)
+        self.data = utils._to_DataFrame(data)

c0ade73def34b154e77ab8baa652cc7b07cf34df
MehdiBits <78798077+MehdiBits@users.noreply.github.com>
2021-08-31 11:00:01 +0200

    Reworked data class and added a draft of profile class

--- src/improve_data_quality.py
+++ src/improve_data_quality.py
@@ -1,15 +1,76 @@
 import utils as utils
 
 
-class ImproveDataQuality:
-    """Implementation of various method for improving data quality of a given data"""
+class Data:
+    """Data class holding a column by column profile and index flagged as low quality data"""
 
-    def __init__(self, data):
-        """
-        Args:
-            data (CSV, JSON, SQL, xlsx): data set.
-        """
-        if utils.check_extension(data) == "none":
-            raise TypeError("data should be of provided as .csv or .json or .sql file")
 
-        self.data = utils._to_DataFrame(data)
+def __init__(self, path):
+    """
+    Args:
+        data (CSV, JSON, SQL): data set.
+    """
+    if utils.check_extension(path) == "none":
+        raise TypeError("data should be of provided as .csv or .json or .sql file")
+
+    self.data = utils._to_DataFrame(path)
+    self._profile = None
+    self._good_index = [range(data.shape[0])]
+    self._bad_index = []
+
+   @property
+    def profile(self):
+        if self._profile is None:
+            raise Exception('profile is None')
+        return self._profile
+    
+    def set_profile(self, df_results):
+         profile = Profiler(self.data)
+         self._profile = profile
+
+    @property
+    def good_index(self):
+        return self._good_index
+
+    @property
+    def bad_index(self):
+        return self._bad_index
+    
+    @bad_index.setter
+    def bad_index(self, list_idx):
+        self._bad_index = list_idx
+
+    @good_index.setter
+    def good_index(self, list_idx):
+        if len(list_idx) > self.data.shape[0]:
+            raise ValueError('Index length must be smaller than the length of the dataframe')
+        self._good_index = list_idx 
+    
+    def push_bad_index(self, list_idx): #Find a better method name
+        for elem in list_idx:
+            try:
+                self.bad_index.append(elem)
+                self.good_index.remove(elem)
+            except:
+                pass
+            
+
+class Profile:
+    """A profile for a dataframe column.
+    """
+
+    def __init__(self, Data, column):
+        self._completeness = 
+        self._size = 
+        self._uniqueness =
+        self._col_type = utils.check_data_types(column)
+        if _col_type == type(str()):
+            pass
+        if _col_type == type(int()) or == type(float()):
+            self._min = Data[column].min()
+            self._max = Data[column].max()
+            self._mean = Data[column].mean()
+            self._std = Data[column].std()
+        self
+
+        
